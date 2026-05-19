f04d51f10d03daf089889ff254249b8d51a6dbcb
KaleabZerihun <Kaleab.Zerihun@jacks.sdstate.edu>
2026-05-19 18:13:07 -0500

    Clean project files

--- bin/Debug/net9.0/p5.asm
+++
@@ -1,9 +0,0 @@
-PROG:     START   0 
-          EXTDEF  ONE,THREE 
-          EXTREF  TWO,FOUR 
-FIRST:    +LDF    TWO 
-          +ADDF   THREE,X 
-          +WD     FOUR+8 
-ONE:      WORD    102 
-THREE:    WORD    ONE-TWO 
-          END     FIRST 
